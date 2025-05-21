0fb14fc9de95787001505f271a1ff739d4bf8cd2
Nico Derito <nicolasderito6@gmail.com>
2025-05-21 20:47:21 -0300

    Actualizo ejercicio TP-04 con líneas faltantes

--- 04 Estructuras Repetitivas/MI-APELLIDO_TP-04.py
+++ 04 Estructuras Repetitivas/MI-APELLIDO_TP-04.py
@@ -64,3 +64,89 @@ while True:
         print("No, intenta de nuevo.")
 
 print("Cantidad de intentos: ", intentos)
+
+#6) Desarrolla un programa que imprima en pantalla todos los números pares comprendidos
+#entre 0 y 100, en orden decreciente.
+
+for numero in range(100 , -1, -1):
+    if numero % 2 == 0:
+        print (numero)
+
+
+#7) Crea un programa que calcule la suma de todos los números comprendidos entre 0 y un
+#número entero positivo indicado por el usuario.
+
+limite = int(input("Ingresa un numero entero positivo: "))
+
+suma = 0
+
+for numero in range(0, limite + 1):
+    suma += numero
+
+    print("La suma de los numeros desde 0 hasta", limite, "es", suma)
+
+
+
+#8) Escribe un programa que permita al usuario ingresar 100 números enteros. Luego, el
+#programa debe indicar cuántos de estos números son pares, cuántos son impares, cuántos son
+#negativos y cuántos son positivos. (Nota: para probar el programa puedes usar una cantidad
+#menor, pero debe estar preparado para procesar 100 números con un solo cambio).
+
+
+cantidad = 100
+
+pares = 0
+impares = 0
+positivos = 0
+negativos = 0
+
+for i in range(cantidad):
+    numero = int(input(f"Ingresa el numero {i + 1}:"))
+
+    if numero % 2 == 0:
+        pares += 1 
+    
+    else:
+        impares += 1
+
+    
+    if numero > 0:
+        positivo += 1
+    elif numero< 0:
+        negativos += 1
+
+    print ("\nRESULTADOS:")
+    print ("Cantidad de numeros pares:", pares)
+    print ("Cantidad de numeros impares:", impares)
+    print ("Cantidad de numeros positivos: ", positivo)
+    print ("Cantidad de numeros positivos:", negativos)
+
+   # 9) Elabora un programa que permita al usuario ingresar 100 números enteros y luego calcule la
+#media de esos valores. (Nota: puedes probar el programa con una cantidad menor, pero debe
+#poder procesar 100 números cambiando solo un valor).
+
+cantidad = 100
+
+
+suma = 0
+
+
+for i in range(cantidad):
+    numero = int(input(f"Ingrésa el número {i + 1}: "))
+    suma += numero
+
+
+media = suma / cantidad
+
+print("\nLa media de los", cantidad, "números ingresados es:", media)
+
+#10) Escribe un programa que invierta el orden de los dígitos de un número ingresado por el
+#usuario. Ejemplo: si el usuario ingresa 547, el programa debe mostrar 745
+
+numero = input("Ingresá un número: ")
+
+numero_invertido = numero[::-1]
+
+print("El número invertido es:", numero_invertido)
+
+print("cant")
